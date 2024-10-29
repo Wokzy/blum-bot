6f4a17719d147793232ef8f3d2912b676fe44e99
Wokzy <yegor6741@gmail.com>
2024-10-29 15:36:48 +0700

    hm changes

--- constants.py
+++ constants.py
@@ -3,6 +3,10 @@ import sys
 """On different devices app size is also different, 
 	so i located triggers on mine and devided it by window size on my pc,
 	in game this coefficients are multiplied by actual window size to get correct coordinates"""
+HALLOWEEN_MODE = '--halloween' in sys.argv
+DOGS_DROP_TOGGLE = '--disable-dogs' not in sys.argv
+
+
 DEV_SCREEN_SIZE_CONST = (402, 712)
 
 APPLICATION_NAME = 'TelegramDesktop'
@@ -15,15 +19,13 @@ APPLICATION_TRIGGER = {"color":(234, 212, 12), "positions":[(60/402, 112/712), (
 PIXELS_PER_ITERATION = 10
 
 NEW_GAME_TRIGGER_POS = (210/402, 615/712)
-AVG_GAME_DURATION = 30 # seconds
+AVG_GAME_DURATION = 30 + 6*HALLOWEEN_MODE # seconds
 
 
 #Dogs drop
 DOGS_WHITE_COLOR_RANGE = (238, 256)
-DOGS_DROP_TOGGLE = '--disable-dogs' not in sys.argv
 
 # Halloween
-HALLOWEEN_MODE = '--halloween' in sys.argv
 HALLOWEEN_COLOR_TRIGGER = {
 					"red":{"min":220, "max":240},
 					"green":{"min":95, "max":130},
